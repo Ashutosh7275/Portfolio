99371e0c0e7cb36086f5e716572a82f0433f1d96
Ashutosh7275 <150644162+Ashutosh7275@users.noreply.github.com>
2024-01-27 10:36:17 -0800

    Add files via upload

---
+++ a.js
@@ -0,0 +1,16 @@
+alert("welcome to my website")
+let a = prompt("enter your username ")
+console.log(`${a}${a.length}@gmail.com`)
+{
+let a = prompt(" enter your correct password ")
+console.log(`${a}`)
+}
+let b = document.getElementById("hire")
+b.addEventListener("dblclick", () => {
+    alert("thanks for hiring me in reputed company and also know about company . And my CV is matched of your information detail ")
+
+})
+let c = document.getElementById("home")
+c.addEventListener("click", () => {
+    alert(" go to the home page")
+})
